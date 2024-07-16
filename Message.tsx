3216d9021370308c9ffcc59a0fa7a9d2608b0fd2
NS258 <154657596+nsgowebjavaprog@users.noreply.github.com>
2024-07-16 10:27:01 +0530

    Add files via upload
    added

---
+++ Message.tsx
@@ -0,0 +1,12 @@
+
+//Pascal-Casting
+function Message(){
+    //JSX:- JavaScript XML
+
+    const name = 'Nagaraj Loni';
+    if(name)
+     return <h1>Hello {name}</h1>;
+    return <h1>Hello Google</h1>;
+}
+
+export default Message;
---
+++ components/ListGroup.tsx
@@ -0,0 +1,28 @@
+import App from "../App";
+import Message from "../Message";
+
+function ListGroup() {
+  let items = ["India", "Karnatak", "Bijapur", "Bangalore"];
+
+  items = [];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
+      {items.length === 0 && <p>No item found </p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(event)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default ListGroup;
