76715477fb8650af1bb534fa85d89bbbb85b8a67
NS258 <154657596+nsgowebjavaprog@users.noreply.github.com>
2024-11-30 10:22:52 +0530

    Add files via upload
    added

---
+++ 294-keeper-app-part-1-completed/src/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
+//6. Make sure that the final website is styled like the example shown here:
+//https://w00gz.csb.app/
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
